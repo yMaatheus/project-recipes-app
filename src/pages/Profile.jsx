9c318758eb365e45a2311bd2c7b90b534ed18900
yMaatheus <stvgamer74@gmail.com>
2022-05-02 17:44:38 -0300

    R82-87: Exibindo na tela de login o email do usuario e criando botões com redirecionamento para outras paginas

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Button } from '../components';
 
 function Profile() {
+  const history = useHistory();
+  const { email } = JSON.parse(localStorage.getItem('user'));
+
+  const handleRedirect = (path) => history.push(path);
+
   return (
     <section>
       <Header title="Profile" />
+      <h2 data-testid="profile-email" style={ { marginTop: '100px' } }>{ email }</h2>
+      <Button
+        label="Done Recipes"
+        onClick={ () => handleRedirect('/done-recipes') }
+        id="profile-done-btn"
+      />
+      <Button
+        label="Favorite Recipes"
+        onClick={ () => handleRedirect('/favorite-recipes') }
+        id="profile-favorite-btn"
+      />
+      <Button
+        label="Logout"
+        onClick={ () => { localStorage.clear(); handleRedirect('/'); } }
+        id="profile-logout-btn"
+      />
       <Footer />
     </section>
   );
