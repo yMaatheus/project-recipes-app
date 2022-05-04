96709dd3c4a58d07508b5bc7d7dda6b5bdf96690
yMaatheus <stvgamer74@gmail.com>
2022-05-04 17:00:50 -0300

    R54: Criando base para fazer pagina de Receitas feitas

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Button } from '../components';
 import Header from '../components/Header';
 
 function DoneRecipes() {
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
   return (
-    <Header title="Done Recipes" />
+    <section>
+      <Header title="Done Recipes" />
+      <section>
+        <Button label="All" onClick={ () => {} } id="filter-by-all-btn" />
+        <Button label="Food" onClick={ () => {} } id="filter-by-food-btn" />
+        <Button label="Drinks" onClick={ () => {} } id="filter-by-drink-btn" />
+      </section>
+      <section>
+        { doneRecipes
+        && doneRecipes.map(({ category, name, image, doneDate, tags }, index) => (
+          <section key={ index }>
+            <img src={ image } alt="" data-testid={ `${index}-horizontal-image` } />
+            <span data-testid={ `${index}-horizontal-top-text` }>{category}</span>
+            <h3 data-testid={ `${index}-horizontal-name` }>{name}</h3>
+            <span data-testid={ `${index}-horizontal-done-date` }>{doneDate}</span>
+            <span data-testid={ `${index}-horizontal-share-btn` }>Compartilhar</span>
+            <ul>
+              { tags.map((tag, tagIndex) => (
+                <li
+                  key={ tagIndex }
+                  data-testid={ `${index}-${tag}-horizontal-tag` }
+                >
+                  {tag}
+                </li>
+              )) }
+            </ul>
+          </section>
+        ))}
+      </section>
+    </section>
   );
 }
 
