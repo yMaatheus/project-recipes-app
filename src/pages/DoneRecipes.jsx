34a30a4940f53ed418215dafb44f45bb96781c54
Leonidas <leonidas.drawanz@gmail.com>
2022-05-05 21:48:47 -0300

    DoneRecipes finished

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,26 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from '../components';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
+
+const copy = require('clipboard-copy');
 
 function DoneRecipes() {
-  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [doneRecipes, setDoneRecipes] = useState('');
+  const [labelButton, setLabelButton] = useState('');
+
+  useEffect(() => {
+    const done = JSON.parse(localStorage.getItem('doneRecipes'));
+    setDoneRecipes(done);
+  }, []);
+
+  const copyToClipBoard = (id, type) => {
+    copy(`http://localhost:3000/${type}s/${id}`);
+    setLabelButton('Link copied!');
+  };
+
+  const applyFilters = (typeFilter) => {
+    const done = JSON.parse(localStorage.getItem('doneRecipes'));
+    if (typeFilter === 'all') {
+      setDoneRecipes(done);
+    }
+    if (typeFilter === 'food') {
+      setDoneRecipes(done.filter(({ type }) => type === typeFilter));
+    }
+    if (typeFilter === 'drink') {
+      setDoneRecipes(done.filter(({ type }) => type === typeFilter));
+    }
+  };
+
   return (
     <section>
       <Header title="Done Recipes" />
-      <section>
-        <Button label="All" onClick={ () => {} } id="filter-by-all-btn" />
-        <Button label="Food" onClick={ () => {} } id="filter-by-food-btn" />
-        <Button label="Drinks" onClick={ () => {} } id="filter-by-drink-btn" />
+      <section style={ { marginTop: '100px' } }>
+        <Button
+          label="All"
+          onClick={ () => applyFilters('all') }
+          id="filter-by-all-btn"
+        />
+        <Button
+          label="Food"
+          onClick={ () => applyFilters('food') }
+          id="filter-by-food-btn"
+        />
+        <Button
+          label="Drinks"
+          onClick={ () => applyFilters('drink') }
+          id="filter-by-drink-btn"
+        />
       </section>
       <section>
         { doneRecipes
-        && doneRecipes.map(({ category, name, image, doneDate, tags }, index) => (
+        && doneRecipes.map(({
+          category, name, image, doneDate, tags, nationality, alcoholicOrNot,
+          id, type,
+        }, index) => (
           <section key={ index }>
-            <img src={ image } alt="" data-testid={ `${index}-horizontal-image` } />
-            <span data-testid={ `${index}-horizontal-top-text` }>{category}</span>
-            <h3 data-testid={ `${index}-horizontal-name` }>{name}</h3>
-            <span data-testid={ `${index}-horizontal-done-date` }>{doneDate}</span>
-            <span data-testid={ `${index}-horizontal-share-btn` }>Compartilhar</span>
+            <Link to={ `/${type}s/${id}` }>
+              <img
+                style={ { width: '300px' } }
+                src={ image }
+                alt=""
+                data-testid={ `${index}-horizontal-image` }
+              />
+              <h3 data-testid={ `${index}-horizontal-name` }>{name}</h3>
+            </Link>
+            <Button
+              onClick={ () => copyToClipBoard(id, type) }
+              id={ `${index}-horizontal-share-btn` }
+              img={ shareIcon }
+              label={ labelButton }
+            />
+            {alcoholicOrNot ? (
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {`${alcoholicOrNot}`}
+              </p>
+            ) : (
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {`${nationality} - ${category}`}
+              </p>
+            )}
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              {`${nationality} - ${category}`}
+            </p>
+            <p data-testid={ `${index}-horizontal-done-date` }>{doneDate}</p>
             <ul>
               { tags.map((tag, tagIndex) => (
                 <li
